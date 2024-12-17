e542e865920c1991e7ffb0cb2321924f40200705
vaishali09107 <127861569+vaishali09107@users.noreply.github.com>
2024-12-17 18:17:18 +0530

    Modifications_done_given_in_PR_as_comments
    This is the modified calculator code

--- Calculator.c
+++ Calculator.c
@@ -1,102 +1,178 @@
 #include <stdio.h>
 #include <ctype.h>
-int main()
+#include<stdlib.h>
+
+
+
+int is_operator(char c)
 {
-    char st[100];
-    printf("Enter the string: ");
-    scanf("%[^\n]",st);
-    int num_arr[100],ind_num=0;
-    char op_arr[100];
-    int ind_op=0;
-    int dig=0;
-    int flg=0;
-    for (int i=0;st[i]!='\0';i++)
+    return (c=='+'||c=='-'||c=='*'||c=='/');
+}
+
+int check_valid_expression(char *expression)
+{
+    int is_previous_operator=0,i;
+    for(i=0; expression[i]!='\0'; i++)
     {
-        char c=st[i];
-        if(c==' ')
+        char c=expression[i];
+        if(is_operator(c))
         {
-            continue;
-        }
-        if(isdigit(c))
-        {
-            dig=dig*10+(c-'0');
-            flg=1;
+            if(is_previous_operator)
+            {
+                return 0;
+            }
+            is_previous_operator=1;
         }
         else
         {
-            if(flg)
+            is_previous_operator=0;
+        }
+    }
+    return 1;
+}
+
+void parse_expression(char *expression, int *number_array,char *operator_array,int *number_index, int *operator_index)
+{
+    int digit=0,flag=0,i;
+    for( i =0; expression[i]!='\0'; i++)
+    {
+        char character=expression[i];
+        if(character==' ')
+        {
+            continue;
+        }
+        if(character>='0' && character<='9')
+        {
+            digit=digit*10+(character-'0');
+            flag=1;
+        }
+        else
+        {
+            if(flag)
             {
-                num_arr[ind_num++]=dig;
-                dig=0;
-                flg=0;
+                number_array[*number_index]=digit;
+                (*number_index)++;
+                digit=0;
+                flag=0;
             }
-            if(c=='+'||c=='-'||c=='*'||c=='/')
+            if(is_operator(character))
             {
-                op_arr[ind_op++]=c;
+                operator_array[*operator_index]=character;
+                (*operator_index)++;
             }
             else
             {
-                printf("Invalid Expression.");
+                printf("Invalid Expression.\n");
+                exit(1);
+            }
+        }
+    }
+    if(flag)
+    {
+        number_array[*number_index] = digit;
+        (*number_index)++;
+    }
+}
+void high_precendence_operations(int *number_array,char *operator_array,int *number_index, int *operator_index)
+{
+    int i,j,k;
+    for(i=0;i< *operator_index;i++)
+    {
+        if(operator_array[i]=='/' || operator_array[i]=='*')
+        {
+            if(operator_array[i]=='/')
+            {
+                if(number_array[i+1]==0)
+                {
+                    printf("Zero Divison Error.\n");
+                    exit(1);
+                }
+                number_array[i]=number_array[i]/number_array[i+1];
+            }
+            else if(operator_array[i]=='*')
+            {
+                number_array[i]=number_array[i]*number_array[i+1];
+            }
+            for(j=i+1;j< *number_index-1;j++)
+            {
+                number_array[j]=number_array[j+1];
+            }
+            for(k=i; k< *operator_index;k++)
+            {
+                operator_array[k]=operator_array[k+1];
+            }
+            (*number_index)--;
+            (*operator_index)--;
+            i--; 
+        }
+    }
+}
+
+int low_precedence_operations(int *number_array,char *operator_array, int operator_index) 
+{
+    int result = number_array[0],i;
+    for (i = 0; i < operator_index; i++) {
+        if (operator_array[i] == '+') 
+        {
+            result = result + number_array[i + 1];
+        } 
+        else if (operator_array[i] == '-') 
+        {
+            result = result - number_array[i + 1];
+        }
+    }
+    return result;
+}
+
+int main()
+{
+    char *expression= NULL,character;
+    int size=0,capacity=20;
+    expression=(char *)malloc(capacity * sizeof(char));
+    if(expression==NULL)
+    {
+        printf("Error in memory allocation.\n");
+        return 1;
+    }
+    printf("Enter the input string: ");
+    while(scanf("%c",&character)==1 && character!='\n')
+    {
+        expression[size++]=character;
+        if (size>=capacity)
+        {
+            capacity*=2;
+            char *temp=realloc(expression,capacity * sizeof(char));
+            if (temp==NULL)
+            {
+                printf("Memory Re-Allocation Failed.\n");
+                free(expression);
                 return 1;
             }
+            expression=temp;
         }
     }
-    if(flg)
+    expression[size]='\0';
+    if(!check_valid_expression(expression))
     {
-        num_arr[ind_num++]=dig;
+        printf("Invalid Expression.\n");
+        free(expression);
+        return 1;
     }
-    for(int j=0;j<ind_op;j++)
+    int *number_array=(int *)malloc(sizeof(int) * capacity);
+    char *operator_array=(char *)malloc(sizeof(char) * capacity);
+    if (number_array==NULL || operator_array==NULL)
     {
-        if(op_arr[j]=='/')
-        {
-            if(num_arr[j+1]==0)
-            {
-                printf("Zero division error.");
-            }
-            num_arr[j]=num_arr[j]/num_arr[j+1];
-            for(int k=j+1;k<ind_num-1;k++)
-            {
-                num_arr[k]=num_arr[k+1];
-            }
-            for(int l=j;l<ind_op-1;l++)
-            {
-                op_arr[l]=op_arr[l+1];
-            }
-            ind_num--;
-            ind_op--;
-            j--;
-        }
+        printf("Memory Allocation Failed.\n");
+        free(expression);
+        return 1;
     }
-    for(int i=0;i<ind_op;i++)
-    {
-        if(op_arr[i]=='*')
-        {
-            num_arr[i]=num_arr[i]*num_arr[i+1];
-            for(int k=i+1;k<ind_num-1;k++)
-            {
-                num_arr[k]=num_arr[k+1];
-            }
-            for(int l=i;l<ind_op-1;l++)
-            {
-                op_arr[l]=op_arr[l+1];
-            }
-            ind_num--;
-            ind_op--;
-            i--;
-        }
-    }
-    int ans=num_arr[0];
-        for(int i=0;i<ind_op;i++)
-        {
-            if(op_arr[i]=='+')
-            {
-                ans=ans+num_arr[i+1];
-            }
-            else if(op_arr[i]=='-')
-            {
-                ans=ans-num_arr[i+1];
-            }
-        }
-    printf("Ans: %d\n",ans);
+    int number_index=0,operator_index=0;
+    parse_expression(expression,number_array,operator_array,&number_index,&operator_index);
+    high_precendence_operations(number_array,operator_array,&number_index,&operator_index);
+    int ans=low_precedence_operations(number_array,operator_array,operator_index);
+    printf("Ans : %d\n",ans);
+    free(expression);
+    free(number_array);
+    free(operator_array);
     return 0;
 }
